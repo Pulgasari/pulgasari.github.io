97143886e2a207f300b2b31eb73b6b13d7ea9880
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-09 03:55:13 +0200

    Update importmap.js

--- importmap.js
+++ importmap.js
@@ -5,6 +5,16 @@
 const pkg = ['compiler', 'hljs'];
 
 const map = { imports: {
+  "@cosmonaut/compiler" : "./cosmonaut/packages/compiler/index.js",
+  "@cosmonaut/ebnf"     : "./cosmonaut/packages/ebnf/index.js",
+  "@cosmonaut/layouter" : "./cosmonaut/packages/layouter/index.js",
+  "@cosmonaut/lsd"      : "./cosmonaut/packages/lsd/index.js",
+  "@cosmonaut/parsers"  : "./cosmonaut/packages/parsers/index.js",
+
+  "@cosmonaut/parsers/"  : "./cosmonaut/packages/parsers/",
+  "@cosmonaut/layouter/" : "./cosmonaut/packages/layouter/",
+  "@cosmonaut/compiler/" : "./cosmonaut/packages/compiler/",
+  
   "@poo/compiler" : "./poo/js-packages/compiler/index.js",
   "@poo/hljs"     : "./poo/js-packages/hljs/index.js",
 }};
