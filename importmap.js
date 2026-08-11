1298b75b32767ea24b118490c906fa768b1b3f2d
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-11 02:40:52 +0200

    Update importmap.js

--- importmap.js
+++ importmap.js
@@ -73,7 +73,12 @@ if (!mapURL) throw new Error('[aufbau] importmap injector must be a classic scri
   );
 
 const createElement = (tag, props) => Object.assign(document.createElement(tag), props);
- 
+
+const PRELOAD_CRITICAL = [
+  '@aufbau/elements',
+  '@aufbau/kits',
+];
+
 // Inject <link rel="modulepreload"> for critical modules
 const fragment = document.createDocumentFragment();
 for (const key of PRELOAD_CRITICAL) {
