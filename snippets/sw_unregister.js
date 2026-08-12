c928b79f46c45491c7053f54d8ed1905b1881907
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-12 15:41:30 +0200

    Create sw_unregister.js

---
+++ snippets/sw_unregister.js
@@ -0,0 +1,8 @@
+// Unregister all active Service Workers for this origin
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
