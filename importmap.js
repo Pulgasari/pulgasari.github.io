3aed9ee4d23bff35913d36705a5aab2a06f95b09
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-25 13:41:48 +0200

    Update importmap.js

--- importmap.js
+++ importmap.js
@@ -56,6 +56,7 @@ const map = { imports: {
   "@poo/compiler" : "./poo/js-packages/compiler/index.js",
   "@poo/hljs"     : "./poo/js-packages/hljs/index.js",
 
+  "@pulgasari/array"  : "./js/array.js",
   "@pulgasari/is"     : "./js/is.js",
   "@pulgasari/logger" : "./js/logger.js",
   "@pulgasari/random" : "./js/random.js",
