a839785626e926ac19e1affc9d63cb7b557cdc71
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-31 11:35:09 +0200

    Update index.js

--- js/modflow/index.js
+++ js/modflow/index.js
@@ -5,5 +5,5 @@ import { Modflow } from './Modflow.js';
 const modflow = new Modflow;
 const mod     = modflow.proxy();
 
-export       { mod, Modflow };
+export       { mod, Modflow, exposeGlobal };
 export default mod;
