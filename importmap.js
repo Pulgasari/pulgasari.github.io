371cbd2bdfe6ab05882a7397ee8481c2baf6fb09
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-19 05:04:10 +0200

    Update importmap.js

--- importmap.js
+++ importmap.js
@@ -85,6 +85,7 @@ const map = { imports: {
   "@pulgasari/canonicalmap" : "./js/canonicalmap.js",
   "@pulgasari/coerce"       : "./js/coerce.js",
   "@pulgasari/hash"         : "./js/hash.js",
+  "@pulgasari/htx"          : "./js-packages/htx/index.js",
   "@pulgasari/is"           : P + 'is',
   "@pulgasari/logger"       : P + 'logger',
   "@pulgasari/num"          : "./js/num.js",
